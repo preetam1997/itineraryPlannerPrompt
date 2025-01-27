bb289fed4440f921e7ba0a86d2f5e590df7c0ff6
preetammukhopadhyay <ompreetam317@gmail.com>
2025-01-27 17:50:40 +0530

    removed unwwanted dependencies

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import random
 import time
 
+from Model import get_model
+
 st.title("Simple chat")
 
 # Initialize chat history
@@ -21,20 +23,16 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
+def response_generator(user_prompt, api_key):
+    model = get_model(0.2,1,1024, api_key)
+    response = model.send_message(user_prompt)
+
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
 
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
-    response = st.write_stream(response_generator())
+    response = st.write_stream(response_generator(prompt, api_key=st.secrets['api_key']))
 # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": response})
