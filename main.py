644fc1a5f86bc9db8163e2d07205cc97393eb853
preetammukhopadhyay <ompreetam317@gmail.com>
2025-01-27 17:19:52 +0530

    removed unwwanted dependencies

--- main.py
+++ main.py
@@ -43,8 +43,16 @@
 # if __name__ == "__main__":
 #     chatbot_interface()
 
+
 import streamlit as st
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
