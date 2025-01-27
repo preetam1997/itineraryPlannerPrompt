195e6bddab78a21760e5f7b6e47b66b12ac45c9e
preetammukhopadhyay <ompreetam317@gmail.com>
2025-01-27 17:24:08 +0530

    removed unwwanted dependencies

--- main.py
+++ main.py
@@ -1,52 +1,8 @@
-# import streamlit as st
-#
-# from Model import get_model
-#
-# API_KEY = st.secrets["api_key"]
-# # Function to generate a simple response (replace with your AI model call)
-# def generate_response(user_input, api_key):
-#     model = get_model(0.2,1,1024, api_key)
-#     chat = model.start_chat()
-#     response = chat.send_message(user_input)
-#     return response.text
-#
-# # Streamlit interface
-# def chatbot_interface():
-#     st.title("Travel Planning Chatbot")
-#
-#     # Initialize the chat history (session state)
-#     if 'chat_history' not in st.session_state:
-#         st.session_state['chat_history'] = []
-#
-#     # User input field
-#     user_input = st.text_input("You: ", "")
-#
-#     # Handle chat when the user submits input
-#     if st.button("Send") and user_input.strip() != "":
-#         # Store user input in the chat history
-#         st.session_state['chat_history'].append(f"You: {user_input}")
-#
-#         # Generate a response (this is where you'd plug in an AI model like Google Gemini)
-#         bot_response = generate_response(user_input, API_KEY)
-#
-#         # Store bot response in the chat history
-#         st.session_state['chat_history'].append(f"Bot: {bot_response}")
-#
-#         # Clear the user input field after sending the message
-#         st.text_input("You: ", "", key="input_text")
-#
-#     # Display the chat history
-#     for message in st.session_state['chat_history']:
-#         st.write(message)
-#
-#
-# if __name__ == "__main__":
-#     chatbot_interface()
-
-
 import streamlit as st
+import random
+import time
 
-st.title("Echo Bot")
+st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -56,3 +12,29 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    response = st.write_stream(response_generator())
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
